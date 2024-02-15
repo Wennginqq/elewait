5c98fec9c671d8009b9d5fa7393ea7621f55553c
Wennginqq <159715319+Wennginqq@users.noreply.github.com>
2024-02-15 03:07:06 +0300

    Add files via upload

--- dip/tablelist.py
+++ dip/tablelist.py
@@ -29,3 +29,21 @@ def endTableSession():
     mysql.connection.commit()
     cursor.close()
     return redirect('/table')
+
+@tableBlueprint.route('/table/orders', methods =['GET', 'POST'])
+def showOrders():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('select * from orders')
+    orders = cursor.fetchall()
+    cursor.close()
+    return render_template('orders.html', orders=orders)
+
+@tableBlueprint.route('/table/aboutorder')
+def aboutOrder():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('select * from orders join employee on orders.employeeId=employee.employeeId where orderId = %s', (request.args.get('orderId'),))
+    order = cursor.fetchone()
+    cursor.execute('select orderId, menu.name, orders_has_menu.ordered_amount from orders join orders_has_menu on orders.orderId=orders_has_menu.orders_orderId join menu on orders_has_menu.menu_foodId=menu.foodId where orderId=%s',(request.args.get('orderId'),))
+    foodInOrder = cursor.fetchall()
+    return render_template('aboutorder.html', order=order, foodInOrder=foodInOrder)
+
